193db04cd4e2caa0353d22bb7227c8148d1be730
mehdigreefhorst <127728353+mehdigreefhorst@users.noreply.github.com>
2023-05-15 17:01:20 +0200

    - Created two classes - class Webscraper --> initializes the whole webscraper with the possiblity to give manual input but can also use the default parameter for testing - Class Webpage --> does a whole webpage evaluating as in the webscraper.py but now it saves it as an object. allowing quick and fast creation of new objects of the same class. - Made the looping work, now it crawls over all url that have a score higher than the threshold. it will keep doing so until loops to execute is finished - to do:       make it so that a link will only be tf idf once by checking if the score is already made before.
    - solved the issue that it keeps looping over the same page
    - changed some code to remove bugs that would make it crash or end early
    - created the start_from_csv & convert_csv_text_to_python data
    - added creat_cswv_file & update_csv_file to the method Webpage
          - made sure that the csv file now also contains the links to visit with similarity with parent
          - added the columns that allow the graph to be made
    - Removed a bug that would not identify the parent_url
    - Addded the visualizer_network.py

---
+++ visualizer_network.py
@@ -0,0 +1,46 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib
+import numpy as np
+
+
+# Read the CSV file
+df = pd.read_csv("/Users/mehdigreefhorst/Desktop/StakeholdersTool/similarity_data_AI_to_check.csv")
+# Create a directed graph
+
+G = nx.MultiDiGraph()
+
+def draw_diagram():
+
+    # Iterate through the DataFrame rows and add nodes and edges to the graph
+    for index, row in df.iterrows():
+        url_link = row['url_link']
+        parent_url = row['parent_url']
+        similarity_score = row['similarity_score']
+        outgoing_link_on_webpage_with_similarity = eval(row["outgoing_link_on_webpage_with_similarity"])
+        """print(outgoing_link_on_webpage_with_similarity[0])
+        print(url_link)
+        print(parent_url)
+        print(similarity_score)"""
+
+
+        # Add nodes with similarity_score as an attribute
+        #G.add_node(url_link, similarity_score=similarity_score)
+
+        # Add edges
+        G.add_edge(parent_url, url_link)
+
+        #links_on_webpage_with_parent = [(parent_url, out_going_link[1]) for out_going_link in outgoing_link_on_webpage_with_similarity]
+        #print(links_on_webpage_with_parent)
+        #G.add_edges_from(links_on_webpage_with_parent, weight = 0.1)
+
+    # Draw the graph
+    pos = nx.spring_layout(G, k=0.15, iterations=20)
+    #labels = nx.get_node_attributes(G, 'similarity_score')
+    #nx.draw_networkx_labels(G, pos, labels=labels)
+    nx.draw_networkx(G, pos, with_labels=False, node_color='skyblue')
+    plt.show()
+
+
+draw_diagram()
